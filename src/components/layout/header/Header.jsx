045d7a338c2309967f8fd42c91095e830cc26709
Vladimir <vokar90@yandex.ru>
2024-05-09 10:31:15 +0300

    feat: настраивает проект

---
+++ src/components/layout/header/Header.jsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 3.2em;
+  line-height: 1.1;
+  font-family: 'Roboto';
+`;
+
+const TestTitle = styled(Title)`
+  text-transform: uppercase;
+`;
+
+export default function Header() {
+  return (
+    <header>
+      <Title>Сборник рецептов из разных стран мира</Title>
+      <TestTitle>qwe qwerty</TestTitle>
+    </header>
+  )
+}
